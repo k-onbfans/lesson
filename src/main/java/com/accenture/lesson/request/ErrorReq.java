791f9c8a015709dcfc7ebb2067787e26619851a7
k-onbfans <809103605@qq.com>
2019-09-23 11:17:10 +0800

    Add Getter , Setter and ToString Remove lombok annotations

--- src/main/java/com/accenture/lesson/request/ErrorReq.java
+++ src/main/java/com/accenture/lesson/request/ErrorReq.java
@@ -1,18 +1,52 @@
 package com.accenture.lesson.request;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import java.sql.Timestamp;
 
-@Getter
-@Setter
-@ToString
 public class ErrorReq {
     private Timestamp timestamp;
     private Integer status;
     private String error;
     private String message;
 
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "ErrorReq{" +
+                "timestamp=" + timestamp +
+                ", status=" + status +
+                ", error='" + error + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/accenture/lesson/request/JosephReq.java
+++ src/main/java/com/accenture/lesson/request/JosephReq.java
@@ -1,15 +1,10 @@
 package com.accenture.lesson.request;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.util.Arrays;
 
-@Getter
-@Setter
-@ToString
+
 public class JosephReq {
 
     @NotNull(message = "elements is empty")
@@ -20,4 +15,37 @@ public class JosephReq {
 
     @Min(value = 1,message = "interval must not less 1")
     private Integer interval;
+
+    public String[] getElements() {
+        return elements;
+    }
+
+    public void setElements(String[] elements) {
+        this.elements = elements;
+    }
+
+    public Integer getStartIndex() {
+        return startIndex;
+    }
+
+    public void setStartIndex(Integer startIndex) {
+        this.startIndex = startIndex;
+    }
+
+    public Integer getInterval() {
+        return interval;
+    }
+
+    public void setInterval(Integer interval) {
+        this.interval = interval;
+    }
+
+    @Override
+    public String toString() {
+        return "JosephReq{" +
+                "elements=" + Arrays.toString(elements) +
+                ", startIndex=" + startIndex +
+                ", interval=" + interval +
+                '}';
+    }
 }
